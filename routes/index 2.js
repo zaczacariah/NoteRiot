3530568a2a4f17e3a7d693c5c998e9f925df96aa
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-03-01 19:38:09 +1030

    removing doubles

--- routes/index 2.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const notesRouter = require('./notes'); // Notes Endpoint
-
-router.use('/notes', notesRouter); // Route to notes endpoint 
-
-module.exports = router;
